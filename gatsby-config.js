1c3f3c8195983899887cb88692a78458cd3775ec
Jay <github@schl.mn>
2024-11-28 21:51:07 +0000

    formspree

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  siteMetadata: {
+    description: "Personal page of Jay Schulman",
+    locale: "en",
+    title: "Jay Schulman",
+    formspreeEndpoint: "https://formspree.io/f/xzzbjqpw",
+  },
   plugins: [
     {
       resolve: "@wkocjan/gatsby-theme-intro",
