cdc9b2936455786614adb63b2d161ccdc148e5af
Jay <github@schl.mn>
2024-12-20 11:40:52 -0600

    add analytics 3

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,14 +4,18 @@ export const onRenderBody = ({ setHeadComponents }) => {
     setHeadComponents([
         <script
             key="reveal-id"
+            src="https://rest.revealid.xyz/v3/script?clientId=3eB2HZ25z3S5XX9dHazKNm&version=4.0.0"
+            async
+        />,
+        <script
+            key="reveal-id-init"
             dangerouslySetInnerHTML={{
                 __html: `
-          !function(){var e='rest.revealid.xyz/v3/script?clientId=3eB2HZ25z3S5XX9dHazKNm&version=4.0.0',
-          t=document.createElement('script');window.location.protocol.split(':')[0];
-          t.src='https://'+e;var c=document.getElementsByTagName('script')[0];
-          t.async = true;
-          t.onload = function(){ new Reveal.default };
-          c.parentNode.insertBefore(t,c)}();
+          window.onload = function() {
+            if (typeof Reveal !== 'undefined') {
+              new Reveal.default();
+            }
+          };
         `
             }}
         />
